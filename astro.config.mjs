14620327018814bdf3593a734c90f8efd0c07849
selma <selmalouise00@gmail.com>
2025-05-25 17:41:36 +0200

    lineup virker med fejl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+// import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: 'server',
+  // adapter: node(),
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ settings.ts
@@ -0,0 +1,4 @@
+export const apiDetails = {
+  supabaseUrl: "https://pdziurppuyuifopvqeap.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkeml1cnBwdXl1aWZvcHZxZWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NzYwNzAsImV4cCI6MjA2MzA1MjA3MH0.pXBiQO37hl7cZE_5eZiChRZMkCIHq9FsUr-LGE_F3Yg",
+};
