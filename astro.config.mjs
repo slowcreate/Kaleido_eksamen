0bfde1be07f1fd4eedfd8d09658195586429a448
selma <selmalouise00@gmail.com>
2025-05-26 21:09:38 +0200

    smårettelser

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-// import node from '@astrojs/node';
 
 export default defineConfig({
-  // output: 'server',
-  // adapter: node(),
   vite: {
     plugins: [tailwindcss()],
   },
